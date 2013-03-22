216fb9bdd6765715b364c0718b59a4df5ba5985e
Michael-John <michael@mjstech.ca>
2013-03-22 19:26:29 -0400

    Workspace changes and added wither sound effect on /trollmode
    Noticed i was commiting my test server, no need for that :P. And
    eclipse was trying to bundle it in the jar :P

--- server/start.sh
+++
@@ -1,1 +0,0 @@
-java -Xms1G -Xmx1G -jar craftbukkit.jar -o true
--- src/me/technogeek48/TrollYourPlayers/TrollYourPlayers.java
+++ src/me/technogeek48/TrollYourPlayers/TrollYourPlayers.java
@@ -1,6 +1,7 @@
 package me.technogeek48.TrollYourPlayers;
 
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -29,7 +30,7 @@ public class TrollYourPlayers extends JavaPlugin {
 			//player.sendMessage("smiting you, your position is: " + pos.toString().replaceAll("{", " "));
 			player.getWorld().strikeLightning(pos).isEffect();
 			return true;
-		} else if (cmd.getName().equalsIgnoreCase("trollmode")) {
+		} else if (cmd.getName().equalsIgnoreCase("trollmode")) { //trollmode cmd
 			int trollmode;
 			Player player = (Player) sender;
 			if(player.getActivePotionEffects().toString().contains("INVISIBILITY")){
@@ -37,27 +38,30 @@ public class TrollYourPlayers extends JavaPlugin {
 			} else {
 				trollmode = 0;
 			}
+			//if the player doesn't have trollmode, give him potion effects and play wither spawn sound.
+			//detect if the player requested the quiet argument, then dont play wither sound.
 			if(trollmode == 0){
-				player.sendMessage("Enabled TrollMode for " + player.getName());
-				player.setAllowFlight(true);
-				player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 10));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 5));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 5));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 5));
-				player.giveExp(9001);
-			}else if(trollmode == 1){
-				player.sendMessage("Disabled trollmode for " + player.getName().toString());
-				player.removePotionEffect(PotionEffectType.INVISIBILITY);
-				player.removePotionEffect(PotionEffectType.SPEED);
-				player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
-				player.removePotionEffect(PotionEffectType.JUMP);
-				player.removePotionEffect(PotionEffectType.REGENERATION);
-				player.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-				player.setAllowFlight(false);
-				//player.removePotionEffect(PotionEffectType.INVISIBILITY);
-				//player.removePotionEffect(PotionEffectType.INVISIBILITY);
+					player.playSound(player.getLocation(), Sound.WITHER_SPAWN, 1, 0);
+					player.sendMessage("Enabled TrollMode for " + player.getName());
+					player.setAllowFlight(true);
+					player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 5));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 5));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 5));
+					player.giveExp(9001);
+			}else if(trollmode == 1){ //if the player already has trollmode, remove the potion effects and play wither death sound.
+					player.playSound(player.getLocation(), Sound.WITHER_DEATH, 1, 0);
+					player.sendMessage("Disabled trollmode for " + player.getName().toString());
+					player.removePotionEffect(PotionEffectType.INVISIBILITY);
+					player.removePotionEffect(PotionEffectType.SPEED);
+					player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+					player.removePotionEffect(PotionEffectType.JUMP);
+					player.removePotionEffect(PotionEffectType.REGENERATION);
+					player.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+					player.setAllowFlight(false);
+				
 			}
 			return true;
 		}
