5191ed3d2970545aac1fa281a33b99929595862e
Michael-John <michael.john.tech@gmail.com>
2013-04-23 20:34:09 -0400

    more reorginizing

--- src/me/technogeek48/TrollYourPlayers/Command/commandFallOfDoom.java
+++ src/me/technogeek48/TrollYourPlayers/Command/commandFallOfDoom.java
@@ -2,9 +2,12 @@ package me.technogeek48.TrollYourPlayers.Command;
 
 import me.technogeek48.TrollYourPlayers.TrollYourPlayers;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class commandFallOfDoom implements CommandExecutor {
 	private TrollYourPlayers plugin;
@@ -14,9 +17,28 @@ public class commandFallOfDoom implements CommandExecutor {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("fallofdoom")){
+			//cast player to sender
+			Player player = (Player) sender;
+			//cast player to args[0]
+			Player target = player.getServer().getPlayer(args[0]);
+			Location targetLocation = target.getLocation();
+			int targetY = targetLocation.getBlockY();
+			int targetX = targetLocation.getBlockX();
+			int targetZ = targetLocation.getBlockZ();
+			float targetYaw = targetLocation.getYaw();
+			float targetPitch = targetLocation.getPitch();
+			//Make a new location with the y axis jacked up.
+			Location newLocation = new Location(target.getWorld(), targetX, targetY + 256, targetZ, targetYaw, targetPitch);
+			if(args.length == 0){
+				player.sendMessage(ChatColor.RED + "Please specify a player!");
+				player.sendMessage(cmd.getUsage().toString());
+			}else{
+			target.teleport(newLocation);
+			}
+		}
+		return true;
 	}
 
 }
