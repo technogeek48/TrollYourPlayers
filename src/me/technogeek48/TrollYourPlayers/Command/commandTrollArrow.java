b6b330e56963b663a7cfba7ee97efc0a9e2660e9
technogeek48 <michael@mjstech.ca>
2013-07-10 15:33:33 -0400

    buggy trollarrrow, will crash the map

--- src/me/technogeek48/TrollYourPlayers/Command/commandTrollArrow.java
+++ src/me/technogeek48/TrollYourPlayers/Command/commandTrollArrow.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 
 public class commandTrollArrow implements CommandExecutor {
@@ -19,12 +20,28 @@ public class commandTrollArrow implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
-		Player target = (player.getServer().getPlayer(args[0]));
+		int arrowStatus = 0;
 		
 		if(cmd.getName().equalsIgnoreCase("trollarrow")){
-			Location arrowTargetLocation = new Location(target.getWorld(), target.getLocation().getBlockX(), target.getLocation().getBlockY() + 15, target.getLocation().getBlockZ());
-			target.getWorld().spawnArrow(arrowTargetLocation, player.getLocation().getDirection(), 10, 0);
+			if(args[0].equalsIgnoreCase("on")){
+				arrowStatus = 1;
+			}else if(args[0].equalsIgnoreCase("off")){
+				arrowStatus = 0;
+			}
+			if(arrowStatus == 1){
+				while (arrowStatus == 1){
+					player.launchProjectile(Arrow.class);
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						break;
+					}
+				}
+			}
 		}
+		
 		return true;
 	}
 
