e2c7623fd5f8e8ed242711fcd99711e4388b9c21
technogeek48 <michael@mjstech.ca>
2014-02-22 20:22:25 -0500

    Restructure

--- src/me/technogeek48/TrollYourPlayers/TrollYourPlayers.java
+++ src/me/technogeek48/TrollYourPlayers/TrollYourPlayers.java
@@ -9,7 +9,6 @@ import me.technogeek48.TrollYourPlayers.Command.commandMobRaid;
 import me.technogeek48.TrollYourPlayers.Command.commandRageQuit;
 import me.technogeek48.TrollYourPlayers.Command.commandSilentSmite;
 import me.technogeek48.TrollYourPlayers.Command.commandSuperSmite;
-import me.technogeek48.TrollYourPlayers.Command.commandTestCommand;
 import me.technogeek48.TrollYourPlayers.Command.commandTrollAnvil;
 import me.technogeek48.TrollYourPlayers.Command.commandTrollArrow;
 import me.technogeek48.TrollYourPlayers.Command.commandTrollBed;
@@ -23,22 +22,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TrollYourPlayers extends JavaPlugin {
 	
 	public void onEnable(){
-		getCommand("trollarrow").setExecutor(new commandTrollArrow(this));
-		getCommand("trollmus").setExecutor(new commandTrollMus(this));
-		getCommand("trollmode").setExecutor(new commandTrollMode(this));
-		getCommand("silentsmite").setExecutor(new commandSilentSmite(this));
-		getCommand("supersmite").setExecutor(new commandSuperSmite(this));
-		getCommand("trollbed").setExecutor(new commandTrollBed(this));
-		getCommand("fallofdoom").setExecutor(new commandFallOfDoom(this));
-		getCommand("trolltp").setExecutor(new commandTrollTp(this));
-		getCommand("ragequit").setExecutor(new commandRageQuit(this));
-		getCommand("trollanvil").setExecutor(new commandTrollAnvil(this));
-		getCommand("anvilpointer").setExecutor(new commandAnvilPointer(this));
-		getCommand("void").setExecutor(new commandVoid(this));
-		getCommand("mobraid").setExecutor(new commandMobRaid(this));
-		getCommand("epicsmite").setExecutor(new commandEpicSmite(this));
+		getCommand("trollarrow") 	  .setExecutor(new commandTrollArrow	  (this));
+		getCommand("trollmus")   	  .setExecutor(new commandTrollMus		  (this));
+		getCommand("trollmode")  	  .setExecutor(new commandTrollMode		  (this));
+		getCommand("silentsmite")	  .setExecutor(new commandSilentSmite	  (this));
+		getCommand("supersmite") 	  .setExecutor(new commandSuperSmite	  (this));
+		getCommand("trollbed")   	  .setExecutor(new commandTrollBed	      (this));
+		getCommand("fallofdoom") 	  .setExecutor(new commandFallOfDoom	  (this));
+		getCommand("trolltp")    	  .setExecutor(new commandTrollTp		  (this));
+		getCommand("ragequit")     	  .setExecutor(new commandRageQuit		  (this));
+		getCommand("trollanvil") 	  .setExecutor(new commandTrollAnvil	  (this));
+		getCommand("anvilpointer")	  .setExecutor(new commandAnvilPointer	  (this));
+		getCommand("void")			  .setExecutor(new commandVoid			  (this));
+		getCommand("mobraid")		  .setExecutor(new commandMobRaid		  (this));
+		getCommand("epicsmite")		  .setExecutor(new commandEpicSmite		  (this));
 		getCommand("epicsmitepointer").setExecutor(new commandEpicSmitePointer(this));
-		getCommand("fakeop").setExecutor(new commandFakeOp(this));
+		getCommand("fakeop")		  .setExecutor(new commandFakeOp          (this));
 	}
 	public void onDisable(){
 		//do shit
